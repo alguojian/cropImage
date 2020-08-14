a8727cd5caca0cdc083e831e3ea13005a9627535
alguojian <jzhszy@foxmail.com>
2020-08-14 18:24:58 +0800

    ..

---
+++ ucrop/src/main/java/com/alguojian/ucrop/callback/CropBoundsChangeListener.java
@@ -0,0 +1,10 @@
+package com.alguojian.ucrop.callback;
+
+/**
+ * Interface for crop bound change notifying.
+ */
+public interface CropBoundsChangeListener {
+
+    void onCropAspectRatioChanged(float cropRatio);
+
+}
